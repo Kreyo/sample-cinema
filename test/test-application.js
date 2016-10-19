e0805495f6593e27f315c1267de8777f3996c084
matvejs timosenko <matvejs.timosenko@accenture.com>
2016-10-19 15:01:53 +0300

    Added a full rendering test for movie list component

--- test/test-application.js
+++ test/test-application.js
@@ -4,12 +4,38 @@ import React from 'react';
 import {Homepage} from '../src/components/pages/homepage';
 import {Terms} from '../src/components/static/terms';
 import {About} from '../src/components/static/about';
+import {MovieListState} from '../src/containers/movies/movie-list';
 import { expect } from 'chai';
+import sinon from 'sinon';
+import jsdom from 'jsdom';
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+const mockStore = configureStore();
 
 describe('<Homepage />', () => {
     it('should have popular releases', () => {
         const wrapper = shallow(<Homepage />);
         expect(wrapper.contains(<h1>Popular Releases</h1>)).to.equal(true);
+    });
+});
+
+describe('<MovieList/>', () => {
+    it('calls componentDidMount', () => {
+        const initialState = {
+            cinemaApp: {
+                movieListState: {
+                    movies: []
+                }
+            }
+        };
+        const store = mockStore(initialState);
+        sinon.spy(MovieListState.prototype, 'componentDidMount');
+        const wrapper = mount(<Provider store={store}><MovieListState /></Provider>);
+        expect(<MovieListState></MovieListState>.prototype.componentDidMount.calledOnce).to.equal(true);
     });
 });
 
